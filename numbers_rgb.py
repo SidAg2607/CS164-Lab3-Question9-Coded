3663240887d97ad8bbde39a501a1646d6ea45ab1
Siddhanth Agrawal <64346340+SidAg2607@users.noreply.github.com>
2020-10-10 03:55:41 +0530

    Update numbers_rgb.py

--- numbers_rgb.py
+++ numbers_rgb.py
@@ -29,17 +29,19 @@ def twodec(onedecx):
     else:
         return onedecx+1
 
-rgb1 = int(input("1st part of rgb color code: "))
-rgb2 = int(input("2nd part of rgb color code: "))
-rgb3 = int(input("3rd part of rgb color code: "))
+def recurse():
+
+  rgb1 = int(input("1st part of rgb color code: "))
+  rgb2 = int(input("2nd part of rgb color code: "))
+  rgb3 = int(input("3rd part of rgb color code: "))
 
   
-binary1 = ""
-binary2 = ""
-binary3 = ""
+  binary1 = ""
+  binary2 = ""
+  binary3 = ""
     
-i = 0
-while(i<8):
+  i = 0
+  while(i<8):
     binary1 = str(rgb1%2) + binary1
     rgb1 = int(rgb1/2)
     binary2 = str(rgb2%2) + binary2
@@ -48,31 +50,37 @@ while(i<8):
     rgb3 = int(rgb3/2)
     i+=1
 
-signmag1 = "".join(bitosm(binary1))
-signmag2 = "".join(bitosm(binary2))
-signmag3 = "".join(bitosm(binary3))
+  signmag1 = "".join(bitosm(binary1))
+  signmag2 = "".join(bitosm(binary2))
+  signmag3 = "".join(bitosm(binary3))
 
-smdec1 = 0
-smdec2 = 0
-smdec3 = 0
+  smdec1 = 0
+  smdec2 = 0
+  smdec3 = 0
 
-j = 0
-while(j<8):
+  j = 0
+  while(j<8):
     smdec1 += int(signmag1[7-j])*(2**j)
     smdec2 += int(signmag2[7-j])*(2**j)
     smdec3 += int(signmag3[7-j])*(2**j)
     j += 1
     
-onedec1 = bitooc(binary1)
-onedec2 = bitooc(binary2)
-onedec3 = bitooc(binary3)
+  onedec1 = bitooc(binary1)
+  onedec2 = bitooc(binary2)
+  onedec3 = bitooc(binary3)
 
-twodec1 = twodec(onedec1)
-twodec2 = twodec(onedec2)
-twodec3 = twodec(onedec3)
+  twodec1 = twodec(onedec1)
+  twodec2 = twodec(onedec2)
+  twodec3 = twodec(onedec3)
 
-print("\nSign Magnitude: " + str(smdec1) + ", " + str(smdec2) + ", " + str(smdec3))
+  print("\nSign Magnitude: " + str(smdec1) + ", " + str(smdec2) + ", " + str(smdec3))
     
-print("\nOne's Complement: " + str(onedec1) + ", " + str(onedec2) + ", " + str(onedec3))
+  print("\nOne's Complement: " + str(onedec1) + ", " + str(onedec2) + ", " + str(onedec3))
     
-print("\nTwo's Complement: " + str(twodec1) + ", " + str(twodec2) + ", " + str(twodec3))
+  print("\nTwo's Complement: " + str(twodec1) + ", " + str(twodec2) + ", " + str(twodec3))
+
+  print("___________________________________________\n")
+
+  recurse()
+
+recurse()
